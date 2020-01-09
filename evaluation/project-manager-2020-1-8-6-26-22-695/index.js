b1d574d64f2f6fb2a788840c4c0182ec426787fc
chuttin <chuttin@163.com>
2020-01-09 18:27:27 +0800

    [Chu] {feat}-{card2}-{AC1-AC5}:finish the menu statistic show

--- evaluation/project-manager-2020-1-8-6-26-22-695/index.js
+++ evaluation/project-manager-2020-1-8-6-26-22-695/index.js
@@ -14,14 +14,14 @@ let listShow = (data) => {
     row.innerHTML = `<td class="name">${ele.name}</td>
     <td class="description" id="description${ele.id}"><p class="showTwoLine">${ele.description}</p></td>
     <td class="endtime">${ele.endTime}</td>
-    <td class="status ${statusJudge(ele.status)}" id="status${ele.id}">${ele.status}</td>
+    <td class="status ${statusToClass(ele.status)}" id="status${ele.id}">${ele.status}</td>
     <td class="operating"><button>删除</button></td>
     `;
     table.appendChild(row);
   });
 }
 
-let statusJudge = (status) => {
+let statusToClass = (status) => {
   let statusClass = '';
   if (status === 'ACTIVE') {
     statusClass = 'active';
@@ -58,7 +58,32 @@ let descriptionAnime = () => {
   descriptionShowTwoLine();
 }
 
+let menuCount = (data) => {
+  let [activeCount, pendingCount, closedCount, sum] = [0, 0, 0, 0];
+  data.map((ele) => {
+    if (ele.status === "ACTIVE") {
+      activeCount += 1;
+    }
+    if (ele.status === "PENDING") {
+      pendingCount += 1;
+    }
+    if (ele.status === "CLOSED") {
+      closedCount += 1;
+    }
+    sum += 1;
+  })
+
+  document.getElementById('pending-count').innerHTML = pendingCount;
+  document.getElementById('active-count').innerHTML = activeCount;
+  document.getElementById('closed-count').innerHTML = closedCount;
+  document.getElementById('all-count').innerHTML = sum;
+  document.getElementById('pending-percent').innerHTML = `${(pendingCount / sum) * 100}%`;
+  document.getElementById('active-percent').innerHTML = `${(activeCount / sum) * 100}%`;
+  document.getElementById('closed-percent').innerHTML = `${(closedCount / sum) * 100}%`;
+}
+
 window.onload = () => {
   getData(listShow);
   descriptionAnime();
+  getData(menuCount);
 }
