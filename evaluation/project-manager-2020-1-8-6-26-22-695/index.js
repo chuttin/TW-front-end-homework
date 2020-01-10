18156343c2158ac47bc0a906273c340f23a233da
chuttin <chuttin@163.com>
2020-01-10 21:29:25 +0800

    [Chu] {fix}-{card3}-{AC4}:the code can't run normally without live server,have fixed it

--- evaluation/project-manager-2020-1-8-6-26-22-695/index.js
+++ evaluation/project-manager-2020-1-8-6-26-22-695/index.js
@@ -1,12 +1,45 @@
-let getData = (callback) => {
+let getData = callback => {
   axios.get('http://localhost:3000/projects')
   .then(function(response) {
     return callback(response.data);
   });
 }
 
-let listShow = (data) => {
+let menuShow = data => {
+  let [activeCount, pendingCount, closedCount, sum] = [0, 0, 0, 0];
+  data.map((ele) => {
+    if (ele.status === "ACTIVE") {
+      activeCount += 1;
+    }
+    if (ele.status === "PENDING") {
+      pendingCount += 1;
+    }
+    if (ele.status === "CLOSED") {
+      closedCount += 1;
+    }
+    sum += 1;
+  });
+
+  document.getElementById('pending-count').innerHTML = pendingCount;
+  document.getElementById('active-count').innerHTML = activeCount;
+  document.getElementById('closed-count').innerHTML = closedCount;
+  document.getElementById('all-count').innerHTML = sum;
+  document.getElementById('pending-percent').innerHTML = `${Math.round((pendingCount / sum) * 100)}%`;
+  document.getElementById('active-percent').innerHTML = `${Math.round((activeCount / sum) * 100)}%`;
+  document.getElementById('closed-percent').innerHTML = `${Math.round((closedCount / sum) * 100)}%`;
+}
+
+let listShow = data => {
   let table = document.getElementById('project-table');
+  table.innerHTML = `<tr>
+    <th class="name">项目名称</th>
+    <th class="description"><p>项目描述</p></th>
+    <th class="endtime">截止时间</th>
+    <th class="status">状态</th>
+    <th class="operating">操作</th>
+  </tr>
+  `;
+
   data.map((ele) => {
     let row = document.createElement('tr');
     row.setAttribute('class', 'projRow');
@@ -15,7 +48,7 @@ let listShow = (data) => {
     <td class="description" id="description${ele.id}"><p class="showTwoLine">${ele.description}</p></td>
     <td class="endtime">${ele.endTime}</td>
     <td class="status ${statusToClass(ele.status)}" id="status${ele.id}">${ele.status}</td>
-    <td class="operating"><button>删除</button></td>
+    <td class="operating"><button onclick="deleteConfirm(this)">删除</button></td>
     `;
     table.appendChild(row);
   });
@@ -24,7 +57,12 @@ let listShow = (data) => {
   descriptionShowTwoLine();
 }
 
-let statusToClass = (status) => {
+let contentShow = (data) => {
+  menuShow(data);
+  listShow(data);
+}
+
+let statusToClass = status => {
   let statusClass = '';
   if (status === 'ACTIVE') {
     statusClass = 'active';
@@ -56,71 +94,51 @@ let descriptionShowTwoLine = () => {
   });
 }
 
-let menuCount = (data) => {
-  let [activeCount, pendingCount, closedCount, sum] = [0, 0, 0, 0];
-  data.map((ele) => {
-    if (ele.status === "ACTIVE") {
-      activeCount += 1;
-    }
-    if (ele.status === "PENDING") {
-      pendingCount += 1;
-    }
-    if (ele.status === "CLOSED") {
-      closedCount += 1;
-    }
-    sum += 1;
-  })
-
-  document.getElementById('pending-count').innerHTML = pendingCount;
-  document.getElementById('active-count').innerHTML = activeCount;
-  document.getElementById('closed-count').innerHTML = closedCount;
-  document.getElementById('all-count').innerHTML = sum;
-  document.getElementById('pending-percent').innerHTML = `${Math.round((pendingCount / sum) * 100)}%`;
-  document.getElementById('active-percent').innerHTML = `${Math.round((activeCount / sum) * 100)}%`;
-  document.getElementById('closed-percent').innerHTML = `${Math.round((closedCount / sum) * 100)}%`;
-}
-
-let deleteProj = (data) => {
-  let deleteButton = document.getElementById('project-table');
-
-  deleteButton.addEventListener('click', (event) => {
-    if (event.target.nodeName === 'BUTTON') {
-      let id = event.target.parentNode.parentNode.id;
-      deleteconfirm(data, id);
-    }
-  });
-}
-
-let deleteconfirm = (data, deleteId) => {
+let deleteConfirm = deleteButton => {
+  let id = deleteButton.parentNode.parentNode.id;
   let deleteconfirm = document.getElementById('delete-win');
   deleteconfirm.style.display = "block";
+  deleteconfirm.name = id;
 
   let bodyEl = document.body;
   let top = window.scrollY;
   bodyEl.style.position = 'fixed';
   bodyEl.style.top = -top + 'px';
 
-  deleteconfirm.addEventListener('click', (event) => {
-    if (event.target.id === 'delete-yes') {
-      data.map((ele) => {
-        if (ele.id === Number(deleteId)) {
-          axios.delete('http://localhost:3000/projects/'+ele.id);
-        }
-      });
-      getData(listShow);
-      getData(menuCount);
-    }
-    if (event.target.id === 'delete-no' || event.target.id === 'cancel-icon' || event.target.id === 'delete-yes') {
-      deleteconfirm.style.display = "none";
-      bodyEl.style.position = '';
-      bodyEl.style.top = '';
-      window.scrollTo(0, top);
-    }
+  let deleteCancel = document.getElementById('cancel-icon');
+  deleteCancel.addEventListener('click', deleteNo);
+}
+
+let deleteYes = confirm => {
+
+  let bodyEl = document.body;
+  let top = window.scrollY;
+  let deleteconfirm = document.getElementById('delete-win');
+  let deleteId = confirm.parentNode.parentNode.name;
+
+  axios.delete(`http://localhost:3000/projects/${deleteId}`).then(() => {
+    let deleteRow = document.getElementById(`${deleteId}`);
+    deleteRow.remove();
+    getData(menuShow);
   });
+  
+  deleteconfirm.style.display = "none";
+  bodyEl.style.position = '';
+  bodyEl.style.top = '';
+  window.scrollTo(0, top);
+}
+
+let deleteNo = () => {
+  let bodyEl = document.body;
+  let top = window.scrollY;
+  let deleteconfirm = document.getElementById('delete-win');
+
+  deleteconfirm.style.display = "none";
+  bodyEl.style.position = '';
+  bodyEl.style.top = '';
+  window.scrollTo(0, top);
 }
 
 window.onload = () => {
-  getData(listShow);
-  getData(menuCount);
-  getData(deleteProj);
+  getData(contentShow);
 }
